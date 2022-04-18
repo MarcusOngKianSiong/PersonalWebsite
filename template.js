35cf446dac8e24469239522c4af0bb3f9dd3e5de
Marcus <marcusongkiansiong@gmail.com>
2022-04-18 18:53:04 +0800

    Improve website design: Metrics, when observed, increases from 0 to a specified number.

--- template.js
+++ template.js
@@ -3,9 +3,9 @@ let section_indicator = document.querySelector('.section_indicator');
 
 let intro = document.querySelector('#intro');
 
+// SECTION INDICATOR
 document.addEventListener('scroll',()=>{
-    let distance = intro.getBoundingClientRect()
-    console.log(distance.top)
+    let distance = intro.getBoundingClientRect()        // Check current distance of top view from the intro section
     if (distance.top<=-2725){
         section_indicator.textContent = categories[3];
         return;
@@ -17,6 +17,7 @@ document.addEventListener('scroll',()=>{
 
     if (distance.top<=-387 && distance.top>=-1000){
         section_indicator.textContent = categories[1];
+        
         return;
     }
 
@@ -29,3 +30,40 @@ document.addEventListener('scroll',()=>{
 
 });
 
+
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+  }
+
+// Measurements -> A visual increase in the number
+let measurements = document.querySelectorAll('.measurement');
+let amount = [60,20,50];                                                        // Set the performance percentage
+function gradual_display_number_increase(element,limit,counter,GradualSpeed,display=0){
+    let interval = setInterval(()=>{
+        // Change speed gradually
+        if (counter!=500){
+            counter+=GradualSpeed;
+        }
+        
+        
+        // display
+        element.textContent = `${display}%`;
+        display+=1;
+
+        clearInterval(interval);
+
+        if(display==limit){
+            return "Limit reached"
+        }
+
+        gradual_display_number_increase(element,limit,counter,GradualSpeed,display)
+    },counter);
+}
+document.addEventListener('scroll',()=>{
+    let distance = intro.getBoundingClientRect();
+    if (distance.top <= -750 && measurements[0].textContent == '0%'){
+        gradual_display_number_increase(measurements[0],amount[0],10,1);
+        gradual_display_number_increase(measurements[1],amount[1],10,1);
+        gradual_display_number_increase(measurements[2],amount[2],10,1);
+    }
+})
